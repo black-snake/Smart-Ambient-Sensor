c38db72f307e177f88ee693a7b32360250e9e4f6
black-snake <black-snake@users.noreply.github.com>
2022-02-05 13:35:19 +0100

    addition to last commit

--- src/utilities/Helpers.cpp
+++ src/utilities/Helpers.cpp
@@ -4,7 +4,32 @@
 
 unsigned long Helpers::_millisOffset;
 
+bool Helpers::_hasWokenUpFromDeepSleep = Helpers::getHasWokenUpFromDeepSleep();
+
 Led Helpers::led;
+
+bool Helpers::getHasWokenUpFromDeepSleep()
+{
+#ifdef ESP32
+    return rtc_get_reset_reason(0) == DEEPSLEEP_RESET;
+#else
+    rst_info *pRstInfo = ESP.getResetInfoPtr();
+
+    return pRstInfo->reason == REASON_DEEP_SLEEP_AWAKE;
+#endif
+}
+
+void Helpers::init()
+{
+#ifdef ESP32
+
+#else
+    if (hasWokenUpFromDeepSleep())
+    {
+        ESP.rtcUserMemoryRead(RTC_MEMORY_OFFSET_MILLIS, (uint32_t *)&_millisOffset, sizeof(_millisOffset));
+    }
+#endif
+}
 
 String Helpers::getChipId()
 {
@@ -18,27 +43,9 @@ String Helpers::getChipId()
     return String("ESP-" + chipId);
 }
 
-bool Helpers::hasStartedFromDeepSleep()
+bool Helpers::hasWokenUpFromDeepSleep()
 {
-#ifdef ESP32
-    return rtc_get_reset_reason(0) == DEEPSLEEP_RESET;
-#else
-    rst_info *pRstInfo = ESP.getResetInfoPtr();
-
-    return pRstInfo->reason == REASON_DEEP_SLEEP_AWAKE;
-#endif
-}
-
-unsigned long Helpers::getMillis()
-{
-#ifdef ESP8266
-    if (hasStartedFromDeepSleep())
-    {
-        ESP.rtcUserMemoryRead(RTC_MEMORY_OFFSET_MILLIS, (uint32_t *)&_millisOffset, sizeof(_millisOffset));
-    }
-#endif
-
-    return millis() + _millisOffset;
+    return _hasWokenUpFromDeepSleep;
 }
 
 void Helpers::setMillisOffset(unsigned long millisOffset)
@@ -48,4 +55,9 @@ void Helpers::setMillisOffset(unsigned long millisOffset)
 #ifdef ESP8266
     ESP.rtcUserMemoryWrite(RTC_MEMORY_OFFSET_MILLIS, (uint32_t *)&_millisOffset, sizeof(_millisOffset));
 #endif
+}
+
+unsigned long Helpers::getMillis()
+{
+    return millis() + _millisOffset;
 }
